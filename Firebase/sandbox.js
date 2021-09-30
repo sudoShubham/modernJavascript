1bde212bc9e3941545b6312108dd8773864a27bc
sudoShubham <shubhamp2017@gmail.com>
2021-09-30 15:48:47 +0530

    Completed project.

---
+++ Firebase/sandbox.js
@@ -0,0 +1,83 @@
+const list = document.querySelector("ul");
+const form = document.querySelector("form");
+
+// db.collection("recipes")
+//   .get()
+//   .then((snapshot) => {
+//     snapshot.docs.forEach((doc) => {
+//       addRecipe(doc.data(), doc.id);
+//       //   console.log(doc.data());
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+db.collection("recipes").onSnapshot((snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    const doc = change.doc;
+    if (change.type === "added") {
+      addRecipe(doc.data(), doc.id);
+    } else if (change.type === "removed") {
+      deleteRecipe(doc.id);
+    }
+  });
+});
+
+const addRecipe = (recipe, id) => {
+  let time = recipe.created_at.toDate();
+
+  let html = `
+    <li data-id="${id}">
+        <div>${recipe.title}</div>
+        <div>${time}</div>
+        <button  class="btn btn-danger btn-sm my-2"  >DELETE</button>
+    </li>
+    
+    `;
+
+  list.innerHTML += html;
+};
+
+const deleteRecipe = (id) => {
+  const recipes = document.querySelectorAll("li");
+  recipes.forEach((recipe) => {
+    if (recipe.getAttribute("data-id") === id) {
+      recipe.remove();
+    }
+  });
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const now = new Date();
+  const recipe = {
+    title: form.recipe.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now),
+  };
+
+  db.collection("recipes")
+    .add(recipe)
+    .then(() => {
+      console.log("Successfully created");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  form.reset();
+});
+
+list.addEventListener("click", (e) => {
+  const id = e.target.parentElement.getAttribute("data-id");
+  //   console.log(id);
+  db.collection("recipes")
+    .doc(id)
+    .delete()
+    .then(() => {
+      console.log("recipe deleted");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
